dab084a7e3796cdf20535da31c05e6b5a9f7bb28
Sirapol Nokyoonthong <pordeetalent@gmail.com>
2018-07-14 20:45:05 +0700

    v2.1
    v2.1

--- sumo/sumo.ino
+++ sumo/sumo.ino
@@ -1,110 +1,91 @@
-//Author Sirapol Nokyoongthong
-//email pordeetalent@gmail.com
-//2018-07-13
+#define trigpinF 0
+#define echopinF 1
+#define trigpinL 2
+#define echopinL 3
+#define trigpinR 4
+#define echopinR 5
 
-#define utf 0 //Ultrasonic trig front
-#define uef 1 //Ultrasonic echo front
-#define utl 2 //Ultrasonic trig left
-#define uel 3 //Ultrasonic echo leftt
-#define utr 4 //Ultrasonic trig right
-#define uer 5 //Ultrasonic echo right
-#define mlia 9 //Motor left in A
-#define mlib 10 //Motor left in B
-#define mlpw 8 //Motor left PWM
-#define mria 12 //Motor right in A
-#define mrib 14 //Motor right in B
-#define mrpw 11 //Motor right PWM
-#define led 13
+int aA1 = 9;
+int bB1 = 10;
+int aA2 = 12;
+int bB2 = 14;
+int en1 = 8;
+int en2 = 11;
 
+void setup()
 
-void setup() {
-  Serial.begin (9600);
-  pinMode(utf, OUTPUT);
-  pinMode(uef, INPUT);
-  pinMode(utl, OUTPUT);
-  pinMode(uel, INPUT);
-  pinMode(utr, OUTPUT);
-  pinMode(uer, INPUT);
-  pinMode(led, OUTPUT);
-  pinMode(mlia, OUTPUT);
-  pinMode(mlib, OUTPUT);
-  pinMode(mlpw, OUTPUT);
-  pinMode(mria, OUTPUT);
-  pinMode(mrib, OUTPUT);
-  pinMode(mrpw, OUTPUT);
+{
+  pinMode(aA1, OUTPUT);
+  pinMode(bB1, OUTPUT);
+  pinMode(aA2, OUTPUT);
+  pinMode(bB2, OUTPUT);
+  pinMode(en1, OUTPUT);
+  pinMode(en2, OUTPUT);
+  pinMode(trigpinF, OUTPUT);
+  pinMode(echopinF, INPUT);
+  pinMode(trigpinL, OUTPUT);
+  pinMode(echopinL, INPUT);
+  pinMode(trigpinR, OUTPUT);
+  pinMode(echopinR, INPUT);
+
 }
 
-void loop() {
-  long duration, distance;
-  long range_f;
-  long range_l;
-  long range_r;
+void loop()
 
-  //Front USonic
-  digitalWrite(utf, LOW);  // Added this line
-  delayMicroseconds(2); // Added this line
-  digitalWrite(utf, HIGH);
-  delayMicroseconds(2); // Added this line
-  range_f = pulseIn(uef, HIGH);
+{
+  int durationF, distanceF;
+  digitalWrite(trigpinF, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigpinF, LOW);
+  durationF = pulseIn(echopinF, HIGH);
+  distanceF = (durationF / 2) / 29.1;
 
-  //Left USonic
-  digitalWrite(utl, LOW);  // Added this line
-  delayMicroseconds(2); // Added this line
-  digitalWrite(utl, HIGH);
-  delayMicroseconds(2); // Added this line
-  range_l = pulseIn(uel, HIGH);
+  if (distanceF >= 40)
 
-  //Right USonic
-  digitalWrite(utr, LOW);  // Added this line
-  delayMicroseconds(2); // Added this line
-  digitalWrite(utr, HIGH);
-  delayMicroseconds(2); // Added this line
-  range_r = pulseIn(uer, HIGH);
+  {
+    digitalWrite(aA1, LOW);
+    digitalWrite(bB1, LOW);
+    digitalWrite(aA2, LOW);
+    digitalWrite(bB2, LOW);
+    analogWrite(en1,0);
+    analogWrite(en2,0);
 
-  //Print
-  Serial.print("rangeF = ");
-  Serial.println(range_f);
-  Serial.print("rangeL = ");
-  Serial.println(range_l);
-  Serial.print("rangeR = ");
-  Serial.println(range_r);
-  Serial.println();
-  Serial.println();
-  
+    delay(500);
 
-
-  if (range_f < 300){
-    digitalWrite(mlia,HIGH);
-    digitalWrite(mlib,HIGH);
-  } else {
-      
   }
 
-  if (range_l < 1000){
-    digitalWrite(mlia,LOW);
-    digitalWrite(mlib,HIGH);
-  } else {
-      
+  else
+
+  {
+
+    if (distanceF >= 25)
+
+    {
+    digitalWrite(aA1, HIGH);
+    digitalWrite(bB1, LOW);
+    digitalWrite(aA2, HIGH);
+    digitalWrite(bB2, LOW);
+    analogWrite(en1,255);
+    analogWrite(en2,255);
+
+    delay(400); 
+
+    }
+
+    else
+
+    {
+    digitalWrite(aA1, LOW);
+    digitalWrite(bB1, HIGH);
+    digitalWrite(aA2, LOW);
+    digitalWrite(bB2, HIGH);
+    analogWrite(en1,255);
+    analogWrite(en2,255);
+
+    delay(400);
+
+    }
+
   }
 
-  if (range_r < 1000){
-    digitalWrite(mlia,HIGH);
-    digitalWrite(mlib,LOW);
-  } else {
-      
-  }
-
-  delay(100);
-
-
 }
-/*
-void fight(){
-  digitalWrite(mlia,HIGH);
-  digitalWrite(mlib,HIGH);
-}
-
-void search(){
-  
-}
-*/
