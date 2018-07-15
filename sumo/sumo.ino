bc7e7c15428728e55c0ded52659b12dc26c2906b
Sirapol Nokyoonthong <pordeetalent@gmail.com>
2018-07-15 09:46:29 +0700

    v2.2
    v2.2

--- sumo/sumo.ino
+++ sumo/sumo.ino
@@ -12,9 +12,7 @@ int bB2 = 14;
 int en1 = 8;
 int en2 = 11;
 
-void setup()
-
-{
+void setup(){
   pinMode(aA1, OUTPUT);
   pinMode(bB1, OUTPUT);
   pinMode(aA2, OUTPUT);
@@ -27,12 +25,10 @@ void setup()
   pinMode(echopinL, INPUT);
   pinMode(trigpinR, OUTPUT);
   pinMode(echopinR, INPUT);
-
 }
 
-void loop()
-
-{
+void loop(){
+  
   int durationF, distanceF;
   digitalWrite(trigpinF, HIGH);
   delayMicroseconds(1000);
@@ -40,49 +36,32 @@ void loop()
   durationF = pulseIn(echopinF, HIGH);
   distanceF = (durationF / 2) / 29.1;
 
-  if (distanceF >= 40)
-
-  {
+  if (distanceF >= 40){
     digitalWrite(aA1, LOW);
     digitalWrite(bB1, LOW);
     digitalWrite(aA2, LOW);
     digitalWrite(bB2, LOW);
     analogWrite(en1,0);
     analogWrite(en2,0);
-
     delay(500);
 
-  }
-
-  else
-
-  {
-
-    if (distanceF >= 25)
-
-    {
-    digitalWrite(aA1, HIGH);
-    digitalWrite(bB1, LOW);
-    digitalWrite(aA2, HIGH);
-    digitalWrite(bB2, LOW);
-    analogWrite(en1,255);
-    analogWrite(en2,255);
-
-    delay(400); 
-
-    }
-
-    else
-
-    {
-    digitalWrite(aA1, LOW);
-    digitalWrite(bB1, HIGH);
-    digitalWrite(aA2, LOW);
-    digitalWrite(bB2, HIGH);
-    analogWrite(en1,255);
-    analogWrite(en2,255);
-
-    delay(400);
+  } else {
+    if (distanceF >= 25){
+      digitalWrite(aA1, HIGH);
+      digitalWrite(bB1, LOW);
+      digitalWrite(aA2, HIGH);
+      digitalWrite(bB2, LOW);
+      analogWrite(en1,255);
+      analogWrite(en2,255);
+      delay(400); 
+    } else {
+      digitalWrite(aA1, LOW);
+      digitalWrite(bB1, HIGH);
+      digitalWrite(aA2, LOW);
+      digitalWrite(bB2, HIGH);
+      analogWrite(en1,255);
+      analogWrite(en2,255);
+      delay(400);
 
     }
 
