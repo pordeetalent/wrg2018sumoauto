245328a4f54c1e94666071a4884e2bcec037d8a2
Sirapol Nokyoonthong <pordeetalent@gmail.com>
2018-07-27 22:15:59 +0700

    v5.3
    v5.3

--- sumo/sumo.ino
+++ sumo/sumo.ino
@@ -10,14 +10,15 @@ const int trigpinR = 4; //trig ของ ultrasonic right
 const int echopinR = 5; //echo ของ ultrasonic right
 const int irL = 8; //infrared left (1=black, 0=white)
 const int irR = 9; //infrared left (1=black, 0=white)
-const int led = 13; //infrared left (1=black, 0=white)
+const int ledL = 12; //LED left
+const int ledR = 13; //LED right
 const int maxDist = 200;
 
 
 const int mla = 15; //มอเตอร์ left ขา inA //หมุนมอเตอร์ซ้ายไปด้านหน้า (ค่ามี 0 กับ 1)
 const int mlb = 14; //มอเตอร์ left ขา inB //หมุนมอเตอร์ซ้ายไปด้านหลัง (ค่ามี 0 กับ 1)
-const int mra = 18; //มอเตอร์ Right ขา inA //หมุนมอเตอร์ขวาไปด้านหน้า (ค่ามี 0 กับ 1)
-const int mrb = 17; //มอเตอร์ Right ขา inB //หมุนมอเตอร์ขวาไปด้านหลัง (ค่ามี 0 กับ 1)
+const int mra = 17; //มอเตอร์ Right ขา inA //หมุนมอเตอร์ขวาไปด้านหน้า (ค่ามี 0 กับ 1)
+const int mrb = 18; //มอเตอร์ Right ขา inB //หมุนมอเตอร์ขวาไปด้านหลัง (ค่ามี 0 กับ 1)
 const int mlpwm = 11; //มอเตอร์ Left ขา pwm //กำหนดความเร็วการหมุนของมอเตอร์ซ้าย (ค่ามี 0 ถึง 255)
 const int mrpwm = 10; //มอเตอร์ Right ขา pwm //กำหนดความเร็วการหมุนของมอเตอร์ขวา (ค่ามี 0 ถึง 255)
 
@@ -30,7 +31,8 @@ NewPing usR(trigpinR, echopinR, maxDist); // Ultrasonic right
 
 void setup(){
   Serial.begin(9600);
-  pinMode(led, OUTPUT);
+  pinMode(ledL, OUTPUT);
+  pinMode(ledR, OUTPUT);
   pinMode(mla, OUTPUT);
   pinMode(mlb, OUTPUT);
   pinMode(mra, OUTPUT);
@@ -48,7 +50,7 @@ void setup(){
 }
 
 void loop(){
-  delay(50);
+  //delay(200);
   
   int usVF = usF.ping_cm();
   int usVL = usL.ping_cm();
@@ -73,19 +75,28 @@ void loop(){
   Serial.println(irLV);
   Serial.print("irR = ");
   Serial.println(irRV);
-  Serial.println("############");
+  Serial.println("############");  
   //##############################
   
 
-  
+  digitalWrite(ledL, LOW);
+  digitalWrite(ledR, LOW);
   if (irLV==0 || irRV==0){
     BACKWARD(80);
     BACKFLIP();
-  } else if (1 <= usVL && usVL <= 10) {
+    digitalWrite(ledL, HIGH);
+    digitalWrite(ledR, HIGH);
+  } else if (usVL >= 1 && usVL <= 10) {
     TURN_L(80);
-  } else if (1 <= usVR && usVR <= 10) {
+    digitalWrite(ledL, HIGH);
+    digitalWrite(ledR, LOW);
+  } else if (usVR >= 1 && usVR <= 10) {
     TURN_R(80);
+    digitalWrite(ledL, LOW);
+    digitalWrite(ledR, HIGH);
   } else {
+    digitalWrite(ledL, LOW);
+    digitalWrite(ledR, LOW);
     FORWARD(80);
   }
   
@@ -99,7 +110,7 @@ void FORWARD (int Speed){
   digitalWrite(mrb, LOW);
   analogWrite(mlpwm, Speed);
   analogWrite(mrpwm, Speed);
-  //delay(500);
+  //delay(50);
 }
 
 void BACKWARD (int Speed){
@@ -109,7 +120,7 @@ void BACKWARD (int Speed){
   digitalWrite(mrb, HIGH);
   analogWrite(mlpwm, Speed);
   analogWrite(mrpwm, Speed);
-  //delay(500);
+  //delay(50);
 }
 
 void TURN_L (int Speed){
@@ -119,7 +130,7 @@ void TURN_L (int Speed){
   digitalWrite(mrb, LOW);
   analogWrite(mlpwm, Speed);
   analogWrite(mrpwm, Speed);
-  //delay(500);
+  //delay(50);
 }
 
 void TURN_R (int Speed){
@@ -129,7 +140,7 @@ void TURN_R (int Speed){
   digitalWrite(mrb, HIGH);
   analogWrite(mlpwm, Speed);
   analogWrite(mrpwm, Speed);
-  //delay(500);
+  //delay(50);
 }
 
 void WAIT (int Speed){
@@ -148,9 +159,10 @@ void BACKFLIP (){
   digitalWrite(mrb, HIGH);
   analogWrite(mlpwm, 80);
   analogWrite(mrpwm, 80);
-  delay(1000);
+  //delay(1000);
 }
 
+/*
 void BLINK (){
   digitalWrite(led, HIGH);
   delay(50);
@@ -159,4 +171,4 @@ void BLINK (){
   digitalWrite(led, HIGH);
   delay(50);
 }
-
+*/
